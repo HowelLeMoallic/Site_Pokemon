cefc523fb1c703f584adf9dcfc37a232943dd456
HowelLeMoallic <howel.lemoallic2022@campus-eni.fr>
2022-10-16 13:32:00 +0200

    MAJ Auto-suggestion
    Création d'une énum avec tous les noms de pokemon pour l'auto-suggestion qui est pratiquement finis, reste plus que le CSS

--- script.js
+++ script.js
@@ -3,23 +3,25 @@
 let datas = [];
 let nbPokemon = 1;
 let numeroPokemon = 0;
+let allPokemon;
 //Fonction permettant de récupérer la saisie pour un nom
 //Et d'aller chercher dans l'API via son nom
-function saisiePokemon() {
+async function saisiePokemon() {
 
-  fetch(`https://pokebuildapi.fr/api/v1/pokemon/`).then(response => response.json()).then(data => research(data));
-
+  fetch(`https://pokebuildapi.fr/api/v1/pokemon/`).then(response => response.json()).then(data =>{ return allPokemon = data});
+  //sessionStorage.setItem('pokemon', data[0].name);
 }
 
 
 function autoSuggestion(elem) {
 
-  let input = elem.value;
-
+  let input = document.getElementById("pokemon");
+  let txt = elem.value;
+  
   let list = document.querySelector(".suggestions");
 
   // Si le texte est vide, alors on arrète la la fonction, et on cache la liste
-  if (!input) {
+  if (!txt) {
     list.style.display = "none";
     return;
   }
@@ -29,9 +31,33 @@ function autoSuggestion(elem) {
   // On créer une variable qui contiendra toute les suggestions qui seront affichées
   frag = document.createDocumentFragment();
   
-  for (var i = 0, c = datas.length + 1; i < c; i++) {
-    // le code qui va venir après
-    console.log(input);
+  for (const element in myEnum) {
+    if (new RegExp("^" + txt, "i").test(`${myEnum[element]}`)) {
+      // On créé l'élément HTML "LI"
+      var word = document.createElement("li");
+      // On l'ajoute au fragment
+      frag.appendChild(word);
+      // On lui mets comme contenu le mot clef avec en gras les lettres entrées
+      // (grâce à une RegEx et à la fonction replace() ^^ )
+      word.innerHTML = `${myEnum[element]}`.replace(new RegExp("^(" + txt + ")", "i"), "<strong>$1</strong>");
+      // On ajoute le mot clef à l'élément HTML "LI", pour pouvoir le récupérer plus tard
+      word.mot = `${myEnum[element]}`;
+
+      // Lorsque le visiteur clique sur le lien
+      word.onmousedown = function () {
+        // On re-sélectionne le champ de texte		
+        input.focus();
+        // On remplace sa valeur par le mot clef
+        input.value = this.mot;
+        // On cache la liste
+        list.style.display = "none";
+
+        // On empêche la dé-sélection du champ
+        return false;
+      };
+      // On indique qu'une suggestion a été ajoutée		
+      suggestions++;
+    }
   }
 
   // Si il y a des suggestions qui peuvent être affichées
@@ -42,6 +68,7 @@ function autoSuggestion(elem) {
     list.appendChild(frag);
     // On affiche la liste
     list.style.display = "block";
+    //input.append(list);
   } // Sinon s'il n'y en a pas
   else {
     // On cache la liste
@@ -49,13 +76,21 @@ function autoSuggestion(elem) {
   }
 
 }
+// Quand l'utilisateur dé-sélectionne le champ
+// (Quand il clique ailleurs sur la page)
+document.getElementById("pokemon").onblur = function(){
+  // On cache la liste
+  document.querySelector(".suggestions").style.display = "none";	
+  
+};
+
 //Fonction pour afficher sur la page
-function research(data) {
+function research() {
 
   //Récupère le champ de saisie de pokemon
   let nomPokemon = document.getElementById("pokemon").value.trim().toLowerCase();
   //Parcours des éléments du tableau 
-  for (const element of data) {
+  for (const element of allPokemon) {
     //Si le nom correspond à l'élément 
     if(nomPokemon == element.name.toLowerCase()){
       //Déclarations
@@ -162,5 +197,406 @@ function infoPokemon(numeroPokemon) {
   });
 }
 
-
+const myEnum = {
+  POKEMON1 : 'Bulbizarre', 
+  POKEMON2 : 'Herbizarre',
+  POKEMON3: 'Florizarre',
+  POKEMON4: 'Salamèche',
+  POKEMON5: 'Reptincel',
+  POKEMON6: 'Dracaufeu',
+  POKEMON7: 'Carapuce',
+  POKEMON8: 'Carabaffe',
+  POKEMON9: 'Tortank',
+  POKEMON10: 'Chenipan',
+  POKEMON11: 'Chrysacier',
+  POKEMON12: 'Papilusion',
+  POKEMON13: 'Aspicot',
+  POKEMON14: 'Coconfort',
+  POKEMON15: 'Dardargnan',
+  POKEMON16: 'Roucool',
+  POKEMON17: 'Roucoups',
+  POKEMON18: 'Roucarnage',
+  POKEMON19: 'Rattata',
+  POKEMON20: 'Rattatac',
+  POKEMON21: 'Piafabec',
+  POKEMON22: 'Rapasdepic',
+  POKEMON23: 'Abo',
+  POKEMON24: 'Arbok',
+  POKEMON25: 'Pikachu',
+  POKEMON26: 'Raichu',
+  POKEMON27: 'Sabelette',
+  POKEMON28: 'Sablaireau',
+  POKEMON29: 'Nidoran♀',
+  POKEMON30: 'Nidorina',
+  POKEMON31: 'Nidoqueen',
+  POKEMON32: 'Nidoran♂',
+  POKEMON33: 'Nidorino',
+  POKEMON34: 'Nidoking',
+  POKEMON35: 'Mélofée',
+  POKEMON36: 'Mélodelfe',
+  POKEMON37: 'Goupix',
+  POKEMON38: 'Feunard',
+  POKEMON39: 'Rondoudou',
+  POKEMON40: 'Grodoudou',
+  POKEMON41: 'Nosferapti',
+  POKEMON42: 'Nosferalto',
+  POKEMON43: 'Mystherbe',
+  POKEMON44: 'Ortide',
+  POKEMON45: 'Rafflesia',
+  POKEMON46: 'Paras',
+  POKEMON47: 'Parasect',
+  POKEMON48: 'Mimitoss',
+  POKEMON49: 'Aéromite',
+  POKEMON50: 'Taupiqueur',
+  POKEMON51: 'Triopikeur',
+  POKEMON52: 'Miaouss',
+  POKEMON53: 'Persian',
+  POKEMON54: 'Psykokwak',
+  POKEMON55: 'Akwakwak',
+  POKEMON56: 'Férosinge',
+  POKEMON57: 'Colossinge',
+  POKEMON58: 'Caninos',
+  POKEMON59: 'Arcanin',
+  POKEMON60: 'Ptitard',
+  POKEMON61: 'Têtarte',
+  POKEMON62: 'Tartard',
+  POKEMON63: 'Abra',
+  POKEMON64: 'Kadabra',
+  POKEMON65: 'Alakazam',
+  POKEMON66: 'Machoc',
+  POKEMON67: 'Machopeur',
+  POKEMON68: 'Mackogneur',
+  POKEMON69: 'Chétiflor',
+  POKEMON70: 'Boustiflor',
+  POKEMON71: 'Empiflor',
+  POKEMON72: 'Tentacool',
+  POKEMON73: 'Tentacruel',
+  POKEMON74: 'Racaillou',
+  POKEMON75: 'Gravalanch',
+  POKEMON76: 'Grolem',
+  POKEMON77: 'Ponyta',
+  POKEMON78: 'Galopa',
+  POKEMON79: 'Ramoloss',
+  POKEMON80: 'Flagadoss',
+  POKEMON81: 'Magnéti',
+  POKEMON82: 'Magnéton',
+  POKEMON83: 'Canarticho',
+  POKEMON84: 'Doduo',
+  POKEMON85: 'Dodrio',
+  POKEMON86: 'Otaria',
+  POKEMON87: 'Lamantine',
+  POKEMON88: 'Tadmorv',
+  POKEMON89: 'Grotadmorv',
+  POKEMON90: 'Kokiyas',
+  POKEMON91: 'Crustabri',
+  POKEMON92: 'Fantominus',
+  POKEMON93: 'Spectrum',
+  POKEMON94: 'Ectoplasma',
+  POKEMON95: 'Onix',
+  POKEMON96: 'Soporifik',
+  POKEMON97: 'Hypnomade',
+  POKEMON98: 'Krabby',
+  POKEMON99: 'Krabboss',
+  POKEMON100: 'Voltorbe',
+  POKEMON101: 'Électrode',
+  POKEMON102: 'Noeunoeuf',
+  POKEMON103: 'Noadkoko',
+  POKEMON104: 'Osselait',
+  POKEMON105: 'Ossatueur',
+  POKEMON106: 'Kicklee',
+  POKEMON107: 'Tygnon',
+  POKEMON108: 'Excelangue',
+  POKEMON109: 'Smogo',
+  POKEMON110: 'Smogogo',
+  POKEMON111: 'Rhinocorne',
+  POKEMON112: 'Rhinoféros',
+  POKEMON113: 'Leveinard',
+  POKEMON114: 'Saquedeneu',
+  POKEMON115: 'Kangourex',
+  POKEMON116: 'Hypotrempe',
+  POKEMON117: 'Hypocéan',
+  POKEMON118: 'Poissirène',
+  POKEMON119: 'Poissoroy',
+  POKEMON120: 'Stari',
+  POKEMON121: 'Staross',
+  POKEMON122: 'M. Mime',
+  POKEMON123: 'Insécateur',
+  POKEMON124: 'Lippoutou',
+  POKEMON125: 'Élektek',
+  POKEMON126: 'Magmar',
+  POKEMON127: 'Scarabrute',
+  POKEMON128: 'Tauros',
+  POKEMON129: 'Magicarpe',
+  POKEMON130: 'Léviator',
+  POKEMON131: 'Lokhlass',
+  POKEMON132: 'Métamorph',
+  POKEMON133: 'Évoli',
+  POKEMON134: 'Aquali',
+  POKEMON135: 'Voltali',
+  POKEMON136: 'Pyroli',
+  POKEMON137: 'Porygon',
+  POKEMON138: 'Amonita',
+  POKEMON139: 'Amonistar',
+  POKEMON140: 'Kabuto',
+  POKEMON141: 'Kabutops',
+  POKEMON142: 'Ptéra',
+  POKEMON143: 'Ronflex',
+  POKEMON144: 'Artikodin',
+  POKEMON145: 'Électhor',
+  POKEMON146: 'Sulfura',
+  POKEMON147: 'Minidraco',
+  POKEMON148: 'Draco',
+  POKEMON149: 'Dracolosse',
+  POKEMON150: 'Mewtwo',
+  POKEMON151: 'Mew',
+  POKEMON152: 'Germignon',
+  POKEMON153: 'Macronium',
+  POKEMON154: 'Méganium',
+  POKEMON155: 'Héricendre',
+  POKEMON156: 'Feurisson',
+  POKEMON157: 'Typhlosion',
+  POKEMON158: 'Kaiminus',
+  POKEMON159: 'Crocrodil',
+  POKEMON160: 'Aligatueur',
+  POKEMON161: 'Fouinette',
+  POKEMON162: 'Fouinar',
+  POKEMON163: 'Hoothoot',
+  POKEMON164: 'Noarfang',
+  POKEMON165: 'Coxy',
+  POKEMON166: 'Coxyclaque',
+  POKEMON167: 'Mimigal',
+  POKEMON168: 'Migalos',
+  POKEMON169: 'Nostenfer',
+  POKEMON170: 'Loupio',
+  POKEMON171: 'Lanturn',
+  POKEMON172: 'Pichu',
+  POKEMON173: 'Mélo',
+  POKEMON174: 'Toudoudou',
+  POKEMON175: 'Togepi',
+  POKEMON176: 'Togetic',
+  POKEMON177: 'Natu',
+  POKEMON178: 'Xatu',
+  POKEMON179: 'Wattouat',
+  POKEMON180: 'Lainergie',
+  POKEMON181: 'Pharamp',
+  POKEMON182: 'Joliflor',
+  POKEMON183: 'Marill',
+  POKEMON184: 'Azumarill',
+  POKEMON185: 'Simularbre',
+  POKEMON186: 'Tarpaud',
+  POKEMON187: 'Granivol',
+  POKEMON188: 'Floravol',
+  POKEMON189: 'Cotovol',
+  POKEMON190: 'Capumain',
+  POKEMON191: 'Tournegrin',
+  POKEMON192: 'Héliatronc',
+  POKEMON193: 'Yanma',
+  POKEMON194: 'Axoloto',
+  POKEMON195: 'Maraiste',
+  POKEMON196: 'Mentali',
+  POKEMON197: 'Noctali',
+  POKEMON198: 'Cornèbre',
+  POKEMON199: 'Roigada',
+  POKEMON200: 'Feuforêve',
+  POKEMON201: 'Zarbi',
+  POKEMON202: 'Qulbutoké',
+  POKEMON203: 'Girafarig',
+  POKEMON204: 'Pomdepik',
+  POKEMON205: 'Foretress',
+  POKEMON206: 'Insolourdo',
+  POKEMON207: 'Scorplane',
+  POKEMON208: 'Steelix',
+  POKEMON209: 'Snubbull',
+  POKEMON210: 'Granbull',
+  POKEMON211: 'Qwilfish',
+  POKEMON212: 'Cizayox',
+  POKEMON213: 'Caratroc',
+  POKEMON214: 'Scarhino',
+  POKEMON215: 'Farfuret',
+  POKEMON216: 'Teddiursa',
+  POKEMON217: 'Ursaring',
+  POKEMON218: 'Limagma',
+  POKEMON219: 'Volcaropod',
+  POKEMON220: 'Marcacrin',
+  POKEMON221: 'Cochignon',
+  POKEMON222: 'Corayon',
+  POKEMON223: 'Rémoraid',
+  POKEMON224: 'Octillery',
+  POKEMON225: 'Cadoizo',
+  POKEMON226: 'Démanta',
+  POKEMON227: 'Airmure',
+  POKEMON228: 'Malosse',
+  POKEMON229: 'Démolosse',
+  POKEMON230: 'Hyporoi',
+  POKEMON231: 'Phanpy',
+  POKEMON232: 'Donphan',
+  POKEMON233: 'Porygon2',
+  POKEMON234: 'Cerfrousse',
+  POKEMON235: 'Queulorior',
+  POKEMON236: 'Debugant',
+  POKEMON237: 'Kapoera',
+  POKEMON238: 'Lippouti',
+  POKEMON239: 'Élekid',
+  POKEMON240: 'Magby',
+  POKEMON241: 'Écrémeuh',
+  POKEMON242: 'Leuphorie',
+  POKEMON243: 'Raikou',
+  POKEMON244: 'Entei',
+  POKEMON245: 'Suicune',
+  POKEMON246: 'Embrylex',
+  POKEMON247: 'Ymphect',
+  POKEMON248: 'Tyranocif',
+  POKEMON249: 'Lugia',
+  POKEMON250: 'Ho-Oh',
+  POKEMON251: 'Celebi',
+  POKEMON252: 'Arcko',
+  POKEMON253: 'Massko',
+  POKEMON254: 'Jungko',
+  POKEMON255: 'Poussifeu',
+  POKEMON256: 'Galifeu',
+  POKEMON257: 'Braségali',
+  POKEMON258: 'Gobou',
+  POKEMON259: 'Flobio',
+  POKEMON260: 'Laggron',
+  POKEMON261: 'Medhyèna',
+  POKEMON262: 'Grahyèna',
+  POKEMON263: 'Zigzaton',
+  POKEMON264: 'Linéon',
+  POKEMON265: 'Chenipotte',
+  POKEMON266: 'Armulys',
+  POKEMON267: 'Charmillon',
+  POKEMON268: 'Blindalys',
+  POKEMON269: 'Papinox',
+  POKEMON270: 'Nénupiot',
+  POKEMON271: 'Lombre',
+  POKEMON272: 'Ludicolo',
+  POKEMON273: 'Grainipiot',
+  POKEMON274: 'Pifeuil',
+  POKEMON275: 'Tengalice',
+  POKEMON276: 'Nirondelle',
+  POKEMON277: 'Hélédelle',
+  POKEMON278: 'Goélise',
+  POKEMON279: 'Bekipan',
+  POKEMON280: 'Tarsal',
+  POKEMON281: 'Kirlia',
+  POKEMON282: 'Gardevoir',
+  POKEMON283: 'Arakdo',
+  POKEMON284: 'Maskadra',
+  POKEMON285: 'Balignon',
+  POKEMON286: 'Chapignon',
+  POKEMON287: 'Parecool',
+  POKEMON288: 'Vigoroth',
+  POKEMON289: 'Monaflèmit',
+  POKEMON290: 'Ningale',
+  POKEMON291: 'Ninjask',
+  POKEMON292: 'Munja',
+  POKEMON293: 'Chuchmur',
+  POKEMON294: 'Ramboum',
+  POKEMON295: 'Brouhabam',
+  POKEMON296: 'Makuhita',
+  POKEMON297: 'Hariyama',
+  POKEMON298: 'Azurill',
+  POKEMON299: 'Tarinor',
+  POKEMON300: 'Skitty',
+  POKEMON301: 'Delcatty',
+  POKEMON302: 'Ténéfix',
+  POKEMON303: 'Mysdibule',
+  POKEMON304: 'Galekid',
+  POKEMON305: 'Galegon',
+  POKEMON306: 'Galeking',
+  POKEMON307: 'Méditikka',
+  POKEMON308: 'Charmina',
+  POKEMON309: 'Dynavolt',
+  POKEMON310: 'Élecsprint',
+  POKEMON311: 'Posipi',
+  POKEMON312: 'Négapi',
+  POKEMON313: 'Muciole',
+  POKEMON314: 'Lumivole',
+  POKEMON315: 'Rosélia',
+  POKEMON316: 'Gloupti',
+  POKEMON317: 'Avaltout',
+  POKEMON318: 'Carvanha',
+  POKEMON319: 'Sharpedo',
+  POKEMON320: 'Wailmer',
+  POKEMON321: 'Wailord',
+  POKEMON322: 'Chamallot',
+  POKEMON323: 'Camérupt',
+  POKEMON324: 'Chartor',
+  POKEMON325: 'Spoink',
+  POKEMON326: 'Groret',
+  POKEMON327: 'Spinda',
+  POKEMON328: 'Kraknoix',
+  POKEMON329: 'Vibraninf',
+  POKEMON330: 'Libégon',
+  POKEMON331: 'Cacnea',
+  POKEMON332: 'Cacturne',
+  POKEMON333: 'Tylton',
+  POKEMON334: 'Altaria',
+  POKEMON335: 'Mangriff',
+  POKEMON336: 'Séviper',
+  POKEMON337: 'Séléroc',
+  POKEMON338: 'Solaroc',
+  POKEMON339: 'Barloche',
+  POKEMON340: 'Barbicha',
+  POKEMON341: 'Écrapince',
+  POKEMON342: 'Colhomard',
+  POKEMON343: 'Balbuto',
+  POKEMON344: 'Kaorine',
+  POKEMON345: 'Lilia',
+  POKEMON346: 'Vacilys',
+  POKEMON347: 'Anorith',
+  POKEMON348: 'Armaldo',
+  POKEMON349: 'Barpau',
+  POKEMON350: 'Milobellus',
+  POKEMON351: 'Morphéo',
+  POKEMON352: 'Kecleon',
+  POKEMON353: 'Polichombr',
+  POKEMON354: 'Branette',
+  POKEMON355: 'Skelénox',
+  POKEMON356: 'Téraclope',
+  POKEMON357: 'Tropius',
+  POKEMON358: 'Éoko',
+  POKEMON359: 'Absol',
+  POKEMON360: 'Okéoké',
+  POKEMON361: 'Stalgamin',
+  POKEMON362: 'Oniglali',
+  POKEMON363: 'Obalie',
+  POKEMON364: 'Phogleur',
+  POKEMON365: 'Kaimorse',
+  POKEMON366: 'Coquiperl',
+  POKEMON367: 'Serpang',
+  POKEMON368: 'Rosabyss',
+  POKEMON369: 'Relicanth',
+  POKEMON370: 'Lovdisc',
+  POKEMON371: 'Draby',
+  POKEMON372: 'Drakhaus',
+  POKEMON373: 'Drattak',
+  POKEMON374: 'Terhal',
+  POKEMON375: 'Métang',
+  POKEMON376: 'Métalosse',
+  POKEMON377: 'Regirock',
+  POKEMON378: 'Regice',
+  POKEMON379: 'Registeel',
+  POKEMON380: 'Latias',
+  POKEMON381: 'Latios',
+  POKEMON382: 'Kyogre',
+  POKEMON383: 'Groudon',
+  POKEMON384: 'Rayquaza',
+  POKEMON385: 'Jirachi',
+  POKEMON386: 'Deoxys',
+  POKEMON387: 'Tortipouss',
+  POKEMON388: 'Boskara',
+  POKEMON389: 'Torterra',
+  POKEMON390: 'Ouisticram',
+  POKEMON391: 'Chimpenfeu',
+  POKEMON392: 'Simiabraz',
+  POKEMON393: 'Tiplouf',
+  POKEMON394: 'Prinplouf',
+  POKEMON395: 'Pingoléon',
+  POKEMON396: 'Étourmi',
+  POKEMON397: 'Étourvol',
+  POKEMON398: 'Étouraptors',
+  POKEMON399: 'Keunotor',
+  POKEMON400: 'Castorno'
+}
 window.onload = saisiePokemon();
